9c2782519a10ea947201f527178fe62d9116281b
user <wodndhksk@gmail.com>
2022-06-07 16:06:36 +0900

    Test

--- mypj/src/main/java/com/example/mypj/indexPage/IndexController.java
+++ mypj/src/main/java/com/example/mypj/indexPage/IndexController.java
@@ -1,7 +1,16 @@
 package com.example.mypj.indexPage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
+    @GetMapping (value="/")
+    public String index(Model model) {
+
+        model.addAttribute("content", "hihi");
+        return "index";
+    }
 }
