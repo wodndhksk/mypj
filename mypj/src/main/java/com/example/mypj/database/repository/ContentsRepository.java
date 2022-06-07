b85bae738280a8038f081b69848f46ded3f02a10
user <wodndhksk@gmail.com>
2022-06-07 09:12:27 +0900

    first commit

---
+++ mypj/src/main/java/com/example/mypj/database/repository/ContentsRepository.java
@@ -0,0 +1,7 @@
+package com.example.mypj.database.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ContentsRepository {
+}
---
+++ mypj/src/main/java/com/example/mypj/indexPage/IndexService.java
@@ -0,0 +1,7 @@
+package com.example.mypj.indexPage;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IndexService {
+}
