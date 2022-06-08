bda698f31b122543abc36f777418f3a620760756
user <wodndhksk@gmail.com>
2022-06-08 15:56:49 +0900

    SecurityConfig Config 어노테이션 추가 및 filter 수정

--- mypj/src/main/java/com/example/mypj/security/SecurityConfig.java
+++ mypj/src/main/java/com/example/mypj/security/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.example.mypj.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     // 정적인 파일에 대한 요청들
     private static final String[] AUTH_WHITELIST = {
@@ -23,35 +32,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //            "/image/**",
 //            "/swagger/**",
 //            "/swagger-ui/**",
+            "/templates/**",
     };
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
+        http
+            .authorizeRequests()
                 // login 없이 접근 허용 하는 url
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**","/admin/**").permitAll()
                 // '/admin'의 경우 ADMIN 권한이 있는 사용자만 접근이 가능
                 .antMatchers("/admin").hasRole("ADMIN")
                 // 그 외 모든 요청은 인증과정 필요
-                .anyRequest().authenticated();
-
-        http.csrf().disable().authorizeRequests();
-
-        http.formLogin()
-                .loginPage("/auth/login")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/auth/signin")
                 .defaultSuccessUrl("/") //로그인 되면 이동될 default URL
-                .permitAll();
-
+                .permitAll()
+                .and()
+            .csrf().disable().authorizeRequests();
     }
     @Override
     public void configure(WebSecurity web) throws Exception {
         // 정적인 파일 요청에 대해 무시
         web.ignoring().antMatchers(AUTH_WHITELIST);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
