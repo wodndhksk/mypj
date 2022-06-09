4015c0274bfdc528b1906a4b83709f41b52760ca
user <wodndhksk@gmail.com>
2022-06-09 16:08:54 +0900

    테스트 계정 insert

---
+++ mypj/src/main/java/com/example/mypj/configuration/DevConfig.java
@@ -0,0 +1,36 @@
+package com.example.mypj.configuration;
+
+import com.example.mypj.database.entity.Account;
+import com.example.mypj.database.entity.AccountType;
+import com.example.mypj.database.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
+
+@Profile("dev")
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class DevConfig {
+    private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
+
+    /**
+     * 테스트 계정 생성
+     */
+    @PostConstruct
+    public void createTestUser(){
+        Account member = Account.builder()
+                .email("test@test.com")
+                .password(passwordEncoder.encode("1234"))
+                .type(AccountType.USER)
+                .build();
+
+        accountRepository.save(member);
+        log.info("TestUser(" + member.getEmail() + ") has been created.");
+    }
+}
