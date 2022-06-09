48df419be9eca35b6fd97e02e528604fbdee7cbd
user <wodndhksk@gmail.com>
2022-06-09 09:45:01 +0900

    HttpRequest 삭제 (InaccessibleObjectException 오류 수정)

--- mypj/src/main/java/com/example/mypj/auth/AuthController.java
+++ mypj/src/main/java/com/example/mypj/auth/AuthController.java
@@ -3,10 +3,9 @@ package com.example.mypj.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.net.http.HttpRequest;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,19 +14,19 @@ public class AuthController {
 
     private final AccountService accountService;
 
-    @RequestMapping(value="/signin")
-    public String signIn(Model model, HttpRequest req) {
+    @GetMapping(value="/signin")
+    public String signIn(Model model) {
 
-        return "auth/signin";
+        return "/auth/signin";
     }
-    @PostMapping(value="/signin")
-    public String afterSignIn(Model model, HttpRequest req) {
-
-        return "auth/signin";
-    }
+//    @PostMapping(value="/auth/signin")
+//    public String afterSignIn(Model model, HttpRequest req) {
+//
+//        return "/auth/signin";
+//    }
     @RequestMapping(value="/signup")
     public String signUp(Model model) {
 
-        return "auth/signup";
+        return "/auth/signup";
     }
 }
