b3170cda0664540362ff7aabc4f7bb26f2836684
user <wodndhksk@gmail.com>
2022-06-09 16:10:24 +0900

    사용 안하는 부분 삭제

--- mypj/src/main/java/com/example/mypj/auth/AuthController.java
+++ mypj/src/main/java/com/example/mypj/auth/AuthController.java
@@ -3,9 +3,7 @@ package com.example.mypj.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
--- mypj/src/main/java/com/example/mypj/configuration/SecurityConfig.java
+++ mypj/src/main/java/com/example/mypj/configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.mypj.security;
+package com.example.mypj.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
