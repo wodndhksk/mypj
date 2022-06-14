cb95cfd302cda89beeb2330840d678b2a571fdc5
user <wodndhksk@gmail.com>
2022-06-14 17:58:03 +0900

    loadUserByUsername 수정

--- mypj/src/main/java/com/example/mypj/configuration/user/UserPrincipalDetailService.java
+++ mypj/src/main/java/com/example/mypj/configuration/user/UserPrincipalDetailService.java
@@ -2,15 +2,10 @@ package com.example.mypj.configuration.user;
 
 import com.example.mypj.database.entity.Account;
 import com.example.mypj.database.repository.AccountRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class UserPrincipalDetailService implements UserDetailsService {
@@ -27,8 +22,7 @@ public class UserPrincipalDetailService implements UserDetailsService {
         if (account == null)
             throw new UsernameNotFoundException(email);
         UserPrincipal userPrincipal = new UserPrincipal(account);
-        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-        authorities.add(new SimpleGrantedAuthority(account.getRoles())); //exception 발생
+
         return userPrincipal;
     }
 }
